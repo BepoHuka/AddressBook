6ab9bd9386fecf93388a5cd09b9783af1c421367
veronika.pasechnik <veronika.pasechnik>
2016-05-16 16:14:32 +0300

    no message

--- src/BookActions.java
+++ src/BookActions.java
@@ -8,17 +8,33 @@ public class BookActions {
 		System.out.println("Enter Name for new record in Address Book: ");
 		String inputString = sc.nextLine();
 		
-		while (!inputString.matches("^([a-z]+[,.]?[ ]?|[a-z]+['-]?)+$")) {
+		while (!inputString.matches("^([A-Z][a-z]+[,.]?[ ]?|[A-Z][a-z]+['-]?)+$")) {
 			System.out.print("Incorrect input. Name could contain only letters from a to z and should started from capital letter: ");
-			inputString = sc.nextLine();     
+			inputString = sc.nextLine();  
 		}
-		 
-		String[] names = new String[inputString];
-		for (int i = 0; i < numDogs; i++)
-	   		if (i < words.length - 1)
-	   			names[i] = words[i + 1]; 
-	   
-		System.out.print("Enter sizes of dogs (small, medium, big) devided by space: ");
+		
+		String[] words = inputString.split("\\s+");	
+		if (inputString.length() > 0) 
+			words = inputString.split("\\s+");
+		else
+			words = new String[0];
+		
+		System.out.print("Enter Phone number for " + inputString + ":");
 		inputString = sc.nextLine();
+		   
+		while (!inputString.matches("^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$")) {    	
+			//This expression matches for all valid in Russia numbers
+			System.out.print("Incorrect input. Phone number could contain only numbers, dashes and brackets: ");
+			inputString = sc.nextLine();
+		}
+	   
+		// Check if entered empty string
+		if (inputString.length() > 0)
+			words = inputString.split("\\s+");
+		else
+			words = new String[0];
+		
 	}
+	
+	
 }
